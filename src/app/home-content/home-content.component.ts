e0d99ca3c685b33559837b51a32b3af34263f065
markokeeffe <markokeeffe@vsware.ie>
2017-11-03 12:35:24 +0000

    Make grids responsive

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home-content',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-content.component.css']
 })
 export class HomeContentComponent implements OnInit {
+  colNum = 6;
+  public innerWidth: any;
 
   constructor() { }
 
+  calculateColumnNumber(){
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth < 550){
+      this.colNum = 1;
+    }
+    else if(this.innerWidth < 980){
+      this.colNum = 4;
+    }
+  }
+
+  @HostListener('window:resize', ['$event']) doSomething(){
+    this.calculateColumnNumber();
+  }
+
   ngOnInit() {
+      this.calculateColumnNumber();
+
   }
 
 }
