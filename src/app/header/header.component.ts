23ec2f08d8d4d211868494d0b0154bd0676dd435
markokeeffe <markokeeffe@vsware.ie>
2017-11-03 16:54:09 +0000

    Nav styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  screens = [
+    {
+      name:'Portraits',
+      path: 'portraits'
+    },
+    {
+      name:'Presentations',
+      path: 'presentations'
+    },
+    {
+      name:'About',
+      path: 'about'
+    },
+    {
+      name:'Contact',
+      path: 'contact'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
