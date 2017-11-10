1433749db7715405f14c333e802fd1b17e7eaffe
markokeeffe <markokeeffe@vsware.ie>
2017-11-10 13:47:16 +0000

    Fix syntax error in presentation service

--- src/app/presentations-service.service.ts
+++ src/app/presentations-service.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class PresentationsServiceService {
-  public data: Array<{name: string, path: string}> = [
-    {
+  public data: Array<{name: string, caption: string, path: string}> = [{
       name: "Mary McAleese",
       caption: "Presentation to the then President of Ireland, Mary McAleese.  Mulhuddert, Dublin, 2009.",
       path:'assets/presentations/maryMcAleese.jpg'
@@ -22,12 +21,13 @@ export class PresentationsServiceService {
       name: "J.P. McManus",
       caption: "Presentation on behalf of Artane Boys Band to J.P. McManus on the occasion of the retirement of Istabraq.  Limerick, 2002.",
       path:'assets/presentations/jpMcManus.jpg'
-    }
+    },
     {
       name: "Pope Benedict XVI",
       caption: "Presentation of portrait by the then Taoiseach, Bertie Ahern, to Pope Benedict XVI.  Vatican, 2005.",
       path:'assets/presentations/popeBenedict.jpg'
     },
+    {
       name: "Smiley Bolger",
       caption: "Smiley Bolger displaying portrait of Phil Lynott to the audience at the 25th Vibe for Philo. Vicar Street, Dublin, 2011.  (Photo courtesy of DigiCol Photography.)",
       path:'assets/presentations/philLynott.jpg'
@@ -51,12 +51,13 @@ export class PresentationsServiceService {
       name: "Ian McKeever",
       caption: "Presentation to world record-breaking mountaineer, Ian McKeever.  Dublin, 2008.",
       path:'assets/presentations/ianMcKeever.jpg'
-    }
+    },
     {
       name: "Jesse Jackson",
       caption: "Presentation to U.S. Civil Rights campaigner, Jesse Jackson.  Dublin, 2004.",
       path:'assets/presentations/jesseJackson.jpg'
     },
+    {
       name: "Bertie Ahern",
       caption: "Presentation to the then Taoiseach, Bertie Ahern.  Dublin, 2003.",
       path:'assets/presentations/bertieAhern.jpg'
@@ -80,12 +81,13 @@ export class PresentationsServiceService {
       name: "Luan Parle",
       caption: "Presentation to Irish musician, Luan Parle.  Dublin, 2008.",
       path:'assets/presentations/luanParle.jpg'
-    }
+    },
     {
       name: "Michael D. Higgins",
       caption: "With my parents, presenting portrait to President Michael D. Higgins &amp; his wife, Sabina. Áras an Uachtaráin, 2013.",
       path:'assets/presentations/michaelDHiggins.jpg'
     },
+    {
       name: "Noam Chomsky",
       caption: "Presentation to linguist and philosopher, Noam Chomsky.  Dublin, 2006.",
       path:'assets/presentations/noamChomsky.jpg'
@@ -109,12 +111,13 @@ export class PresentationsServiceService {
       name: "Norman Wisdom",
       caption: "Presentation to legendary British comedian, Norman Wisdom.  Dublin, 2004.",
       path:'assets/presentations/normanWisdom.jpg'
-    }
+    },
     {
       name: "John Hume",
       caption: "Presentation to Nobel Laureate, John Hume.  Dublin, 2001.",
       path:'assets/presentations/johnHume.jpg'
     },
+    {
       name: "Smiley Bolger",
       caption: "Presentation to Smiley Bolger.  Dublin 2010.",
       path:'assets/presentations/smileyBolger.jpg'
@@ -130,16 +133,15 @@ export class PresentationsServiceService {
       path:'assets/presentations/tkWhitaker.jpg'
     },
     {
-      name: "F.W. De Klerk"",
+      name: "F.W. De Klerk",
       caption: "Presentation to the former President of South Africa, F.W. De Klerk.  Dublin, 2004.",
       path:'assets/presentations/fwDeKlerk.jpg'
-    }
+    },
     {
       name: "Chris Hadfield",
       caption: "Presentation to astronaut, Col. Chris Hadfield, on behalf of Eason, O'Connell Street.  Dublin, 2014.",
       path:'assets/presentations/chrisHadfield.jpg'
-    }
-  ]
+    }]
   constructor() { }
 
 }
